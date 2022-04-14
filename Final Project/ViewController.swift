896366f5e7d722bd12a8713d74d44a3351ad3564
Akatsuki2345 <abelxucanada@gmail.com>
2022-04-14 17:38:42 -0400

    UI Start up

--- Final Project/ViewController.swift
+++ Final Project/ViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputname: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     
+    
+    
 
-
+    @IBAction func click(_ sender: Any) {
+        let username = inputname.text ?? ""
+        
+    }
+    
 }
 
