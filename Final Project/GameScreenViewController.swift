38b4a23cc32402dadcd357c47b36b932b5ebb865
Akatsuki2345 <abelxucanada@gmail.com>
2022-04-14 20:40:53 -0400

    shuffle function at the beginning

--- Final Project/GameScreenViewController.swift
+++ Final Project/GameScreenViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 class GameScreenViewController: UIViewController {
 
 
-    @IBOutlet weak var userscore: UILabel!
-    @IBOutlet weak var housescore: UILabel!
+    @IBOutlet weak var userscorelab: UILabel!
+    @IBOutlet weak var housescorelab: UILabel!
     
     @IBOutlet weak var housecard1: UIImageView!
     
@@ -30,9 +30,26 @@ class GameScreenViewController: UIViewController {
     }
     
     @IBAction func hit(_ sender: Any) {
+        var pokerkind = ["Club","Spade","Diamond","Heart"]
+        pokerkind.shuffle()
+        let newpokerkind = pokerkind.shuffled()
+        var cardnumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+        cardnumber.shuffle()
+        let newcardnumber = cardnumber.shuffled()
         
+        housecard1.image = UIImage(named: "Card\(newpokerkind[0])\(newcardnumber[0])")
+        housecard2.image = UIImage(named: "Card\(newpokerkind[1])\(newcardnumber[1])")
+        let housescore = newcardnumber[0] + newcardnumber[1]
+        housescorelab.text = "\(housescore)"
+        usercard1.image = UIImage(named: "Card\(newpokerkind[2])\(newcardnumber[2])")
+        usercard2.image = UIImage(named: "Card\(newpokerkind[3])\(newcardnumber[3])")
+        let userscore = newcardnumber[2] + newcardnumber[3]
+        userscorelab.text = "\(userscore)"
     }
    
+    @IBAction func hitafterstart(_ sender: Any) {
+        
+    }
     @IBAction func stay(_ sender: Any) {
         
     }
